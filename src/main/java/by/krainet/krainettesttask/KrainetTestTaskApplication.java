b54677bd7d343c33afbb69816d215d945ab11897
ilyamoiseenko <ilya.moiseenko.00@yandex.by>
2024-07-29 17:49:13 +0300

    initial

---
+++ src/main/java/by/krainet/krainettesttask/KrainetTestTaskApplication.java
@@ -0,0 +1,13 @@
+package by.krainet.krainettesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrainetTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrainetTestTaskApplication.class, args);
+    }
+
+}
