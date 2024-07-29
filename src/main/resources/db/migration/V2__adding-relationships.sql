3f1a16ade4cef44d4e7a02b169d1a7f031521197
ilyamoiseenko <ilya.moiseenko.00@yandex.by>
2024-07-29 19:05:54 +0300

    Add migrations

---
+++ src/main/resources/db/migration/V2__adding-relationships.sql
@@ -0,0 +1,17 @@
+-- relationship many to many for user and role
+CREATE TABLE IF NOT EXISTS user_role (
+    fk_user_id BIGINT NOT NULL,
+    fk_role_id BIGINT NOT NULL,
+
+    PRIMARY KEY (fk_user_id, fk_role_id),
+    CONSTRAINT fk_user_role_user FOREIGN KEY (fk_user_id) REFERENCES tb_user(id),
+    CONSTRAINT fk_user_role_role FOREIGN KEY (fk_role_id) REFERENCES tb_role(id)
+);
+
+-- relationship one to  many for record and user
+ALTER TABLE tb_record ADD COLUMN fk_user_id BIGINT;
+ALTER TABLE tb_record ADD CONSTRAINT fk_record_user FOREIGN KEY (fk_user_id) REFERENCES tb_user(id);
+
+-- relationship one to many for record and project
+ALTER TABLE tb_record ADD COLUMN fk_project_id BIGINT;
+ALTER TABLE tb_record ADD CONSTRAINT fk_record_project FOREIGN KEY (fk_project_id) REFERENCES tb_project(id);
