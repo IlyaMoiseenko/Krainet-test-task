feee311c03d9e8eed721c0ab4047257c621b67b7
ilyamoiseenko <ilya.moiseenko.00@yandex.by>
2024-07-29 21:03:38 +0300

    Added default admin in application

--- src/main/java/by/krainet/krainettesttask/KrainetTestTaskApplication.java
+++ src/main/java/by/krainet/krainettesttask/KrainetTestTaskApplication.java
@@ -2,25 +2,49 @@ package by.krainet.krainettesttask;
 
 import by.krainet.krainettesttask.common.Roles;
 import by.krainet.krainettesttask.domain.Role;
+import by.krainet.krainettesttask.domain.User;
 import by.krainet.krainettesttask.repository.RoleRepository;
+import by.krainet.krainettesttask.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+import java.util.Set;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class KrainetTestTaskApplication {
+
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(KrainetTestTaskApplication.class, args);
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+    public CommandLineRunner commandLineRunner() {
         return args -> {
             if (roleRepository.findByName(Roles.USER.getName()).isEmpty() && roleRepository.findByName(Roles.ADMIN.getName()).isEmpty()) {
                 roleRepository.save(Role.builder().name(Roles.ADMIN.getName()).build());
                 roleRepository.save(Role.builder().name(Roles.USER.getName()).build());
+            }
+
+            if (userRepository.findByUsername(Roles.ADMIN.getName()).isEmpty()) {
+                Optional<Role> role = roleRepository.findByName(Roles.ADMIN.getName());
+                User user = User
+                        .builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        .roles(Set.of(role.get()))
+                        .build();
+
+                userRepository.save(user);
             }
         };
     }
