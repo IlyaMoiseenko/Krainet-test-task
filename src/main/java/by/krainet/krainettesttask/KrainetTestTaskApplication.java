ecffca94cc2a67de5f3b3687ea1c5ee06353524f
ilyamoiseenko <ilya.moiseenko.00@yandex.by>
2024-07-29 20:47:13 +0300

    Add default roles in application

--- src/main/java/by/krainet/krainettesttask/KrainetTestTaskApplication.java
+++ src/main/java/by/krainet/krainettesttask/KrainetTestTaskApplication.java
@@ -1,7 +1,12 @@
 package by.krainet.krainettesttask;
 
+import by.krainet.krainettesttask.common.Roles;
+import by.krainet.krainettesttask.domain.Role;
+import by.krainet.krainettesttask.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KrainetTestTaskApplication {
@@ -10,4 +15,13 @@ public class KrainetTestTaskApplication {
         SpringApplication.run(KrainetTestTaskApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findByName(Roles.USER.getName()).isEmpty() && roleRepository.findByName(Roles.ADMIN.getName()).isEmpty()) {
+                roleRepository.save(Role.builder().name(Roles.ADMIN.getName()).build());
+                roleRepository.save(Role.builder().name(Roles.USER.getName()).build());
+            }
+        };
+    }
 }
