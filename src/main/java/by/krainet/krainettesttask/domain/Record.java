439d24da951ad70673ed187dc1e6a0976e26592a
ilyamoiseenko <ilya.moiseenko.00@yandex.by>
2024-07-30 20:42:15 +0300

    Add JsonFormat for LocalDateTime fields to improve the date display

--- src/main/java/by/krainet/krainettesttask/domain/Record.java
+++ src/main/java/by/krainet/krainettesttask/domain/Record.java
@@ -1,5 +1,6 @@
 package by.krainet.krainettesttask.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -16,9 +17,11 @@ import java.time.LocalDateTime;
 public class Record extends BaseDomain {
 
     @Column(name = "start_time")
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
     private LocalDateTime startTime;
 
     @Column(name = "end_time")
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
     private LocalDateTime endTime;
 
     @ManyToOne
